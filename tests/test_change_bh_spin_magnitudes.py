8a735c237e092f6d6dd9d1fb9cfd7164d4696da6
K. E. Saavik Ford <sford@amnh.org>
2025-02-03 10:50:34 -0500

    non-working test

--- tests/test_change_bh_spin_magnitudes.py
+++ tests/test_change_bh_spin_magnitudes.py
@@ -4,20 +4,36 @@ import pytest
 
 from mcfacts.physics import accretion
 
+# Taken from <https://stackoverflow.com/a/9098295/4761692>
+def named_product(**items):
+    Options = collections.namedtuple('Options', items.keys())
+    return itertools.starmap(Options, itertools.product(*items.values()))
+
 def param_change_bh_spin_magnitudes():
     """return input and expected values"""
     
-    disk_bh_pro_orbs_ecc = np.array([0.0, 0.01, 0.03, 0.3, 0.5, 0.7, 0.9])
-    disk_bh_pro_spins = 0.7 * np.ones(len(disk_bh_pro_orbs_ecc))
-    # actually want to test on spins -1.0, -0.7, -0.1, 0.0, 0.1, 0.7, 1.0
-    disk_bh_eddington_ratio = 0.5
+    TEST_ECCENTRICITIES = np.array([0.0, 0.01, 0.03, 0.3, 0.5, 0.7, 0.9])
+    TEST_SPINS = np.array([-1.0, -0.7, -0.1, 0.0, 0.1, 0.7, 1.0])
+    singles = named_product(
+        disk_bh_pro_orbs_ecc = TEST_ECCENTRICITIES,
+        disk_bh_pro_spins = TEST_SPINS,
+    )
+    singles_array = np.zeros(TEST_ECCENTRICITIES.size * TEST_SPINS.size, dtype=float)
+    for i,item in enumerate(singles):
+        singles_array[i] = [item.disk_bh_pro_orbs_ecc,item.disk_bh_pro_spins]
+
+    disk_bh_eddington_ratio = np.array([0.01, 0.1, 0.5, 1.0, 5.0])
     # actually want to test on [0.01, 0.1, 0.5, 1.0, 5.0]
-    disk_bh_torque_condition = 0.01
+    disk_bh_torque_condition = [0.01, 0.1]
     # actually want to test on [0.01, 0.1]
-    timestep_duration_yr = 1e4
+    timestep_duration_yr = [1e2, 1e3, 1e4, 1e5]
     # actually want to test on [1e2, 1e3, 1e4, 1e5]
-    disk_bh_pro_orbs_ecc_crit = 0.03
+    disk_bh_pro_orbs_ecc_crit = [0.03]
     # this is fine as long as ecc array remains same--want a crit ecc in the middle
+
+    config_disk_vars = named_product(
+        disk_bh_eddington_ratio = 
+    )
     
     "Why aren't the masses passed in, if we need to know the fraction of initial mass accreted?"
 
