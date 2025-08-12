7986a49ba3e79a2494184dd5deaf4038493144f7
shray4 <sray@gradcenter.cuny.edu>
2025-08-12 00:30:22 -0400

    okay this time i ACTUALLY fixed the pytest yipppeee ^_^

--- tests/test_change_bh_spins.py
+++ tests/test_change_bh_spins.py
@@ -4,33 +4,36 @@ import conftest as provider
 from conftest import InputParameterSet
 import mcfacts.physics.accretion as accretion
 
+#def accretion_bh_spin_angle_params():
+    #return input and expected values
+    #disk_bh_spin_angle = provider.INPUT_PARAMETERS["bh_spin_angles"][InputParameterSet.SINGLETON]
+    #disk_bh_orbital_eccentricity = provider.INPUT_PARAMETERS["bh_orbital_eccentricity"][InputParameterSet.SINGLETON]
+
+    #expected_spin_angle = [0.0, 0.34906585, 0.6981317,  1.04719755, 1.3962634,  1.74532925, 2.0943951,  2.44346095, 2.7925268,  3.14159265]
+    
+    #return zip(disk_bh_spin_angle, disk_bh_orbital_eccentricity, expected_spin_angle)
+
+#@pytest.mark.parametrize("disk_bh_spin_angle, disk_bh_orbital_eccentricity, expected_spin_angle", accretion_bh_spin_angle_params())
+
 def accretion_bh_spin_params():
     #return input and expected values
     disk_bh_spin = provider.INPUT_PARAMETERS["bh_spins"][InputParameterSet.SINGLETON]
-    disk_bh_orbital_eccentricity = provider.INPUT_PARAMETERS["bh_orbital_eccentricity"][InputParameterSet.SINGLETON]
-
-    expected_spin = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-
-    return zip(disk_bh_spin, disk_bh_orbital_eccentricity, expected_spin)
-
-def accretion_bh_spin_angle_params():
-    #return input and expected values
     disk_bh_spin_angle = provider.INPUT_PARAMETERS["bh_spin_angles"][InputParameterSet.SINGLETON]
     disk_bh_orbital_eccentricity = provider.INPUT_PARAMETERS["bh_orbital_eccentricity"][InputParameterSet.SINGLETON]
 
+    expected_spin = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     expected_spin_angle = [0.0, 0.34906585, 0.6981317,  1.04719755, 1.3962634,  1.74532925, 2.0943951,  2.44346095, 2.7925268,  3.14159265]
-    
-    return zip(disk_bh_spin_angle, disk_bh_orbital_eccentricity, expected_spin_angle)
 
-@pytest.mark.parametrize("disk_bh_spin, disk_bh_orbital_eccentricity, expected_spin", accretion_bh_spin_params())
-@pytest.mark.parametrize("disk_bh_spin_angles, disk_bh_orbital_eccentricity, expected_spin_angle", accretion_bh_spin_angle_params())
+    return zip(disk_bh_spin, disk_bh_spin_angle, disk_bh_orbital_eccentricity, expected_spin, expected_spin_angle)
 
-def test_change_bh_spin(disk_bh_spin, disk_bh_pro_spin_angles, disk_bh_orbital_eccentricity, expected_spin, expected_spin_angle):
+@pytest.mark.parametrize("disk_bh_spin, disk_bh_spin_angle, disk_bh_orbital_eccentricity, expected_spin, expected_spin_angle", accretion_bh_spin_params())
+
+def test_change_bh_spin(disk_bh_spin, disk_bh_spin_angle, disk_bh_orbital_eccentricity, expected_spin, expected_spin_angle):
     """test change_bh_spin function"""
 
     change_bh_spin, change_bh_spin_angle = accretion.change_bh_spin(
         np.array([disk_bh_spin]),
-        np.array([disk_bh_pro_spin_angles]),
+        np.array([disk_bh_spin_angle]),
         1.0,
         0.1,
         0.02,
