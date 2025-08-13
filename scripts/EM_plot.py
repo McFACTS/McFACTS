a68d686a3b4e2f2990168a35387daaf109669135
Varun Pritmani <varunprit18@gmail.com>
2025-08-13 14:13:10 -0400

    style: improve script structure

--- scripts/EM_plot.py
+++ scripts/EM_plot.py
@@ -1,22 +1,110 @@
 import numpy as np
 import pandas as pd
 from astropy.io import fits
-from astropy.constants import h, c, k_B, m_p
+from astropy.constants import h, c, k_B, m_p, R_sun, au
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import mcfacts
 
+# Parameters that you can change for new model
+c_factor = 0.1 #factor to change the speed of light for the velocity of forward shock
+v_fs = c_factor * c.value #velocity of forward shock in m/s
+dens = 1e-15 #density of agn disk
+
 # Constants used 
 #t = 5800 #Temp of sun for now can change for what we are looking at 
-rad = 6.96e8 #Radius of sun in meters for now will change to bh later
-d = 1.5e11 #distance from earth to sun in meters 
+rad = R_sun #Radius of sun in meters for now will change to bh later
+d = au #distance from earth to sun in meters change later to distance to agn
 h = h.value #Planck constant
 c = c.value #speed of light
 k = k_B.value #Boltzmann constant
 m_p = m_p.cgs.value #proton mass
 pi = np.pi #Pi constant
-v_fs = 0.1 * c #velocity of forward shock (used in Beta)
-dens = 1e-15 #density of agn disk
+
+# Function to calculate temperature based on velocity of forward shock and density
+def temp_fxn(v_fs, dens, m_p, c, k):
+    """
+    Calculate the temperature t based on velocity of forward shock and other physical parameters. All parameters should be in CGS units
+
+    Parameters
+    ----------
+    v_fs : float
+        Velocity of the forward shock in m/s.
+    dens : float
+        Density of AGN disk.
+    m_p : float
+        Proton mass.
+    c : float
+        Speed of light.
+    k : float
+        Boltzmann constant.
+
+    Returns
+    -------
+    t : float
+        Calculated temperature.
+    """
+    b_fs = v_fs / c
+    gamma_fs = 1 / np.sqrt(1 - b_fs**2)
+    gamma_fs_f = gamma_fs**(1 + np.sqrt(3))
+    gamma_sf = gamma_fs / np.sqrt(2)
+    gamma_sf_f = gamma_sf**(1 + np.sqrt(3))
+    n = dens / m_p
+
+    if b_fs * gamma_fs <= 0.03:
+        t = 1e4 * ((b_fs / 0.02)**0.5) * ((dens / 1e-16)**0.25)
+    elif b_fs * gamma_fs > 0.03 and b_fs * gamma_fs <= 1:
+        t = (10**(0.975 + (1.735 * ((b_fs / 0.1)**0.5)) + ((0.26 - 0.08 * ((b_fs / 0.1)**0.5)) * np.log10(n / 1e15)))) * 1e4
+    elif b_fs * gamma_fs > 1:
+        t = (gamma_sf_f * 5e4) / k
+    if b_fs >= 1:
+        print("Velocity is greater than speed of light, using beta = 0.98 instead")
+        b_fs = 0.98
+        gamma_fs = 1 / np.sqrt(1 - b_fs**2)
+        gamma_sf = gamma_fs / np.sqrt(2)
+        gamma_sf_f = gamma_sf**(1 + np.sqrt(3))
+        t = (gamma_sf_f * 5e4) / k
+    return t
+
+# Plancks law for frequency in graph
+def planck_fxn(filter, t):
+    """Planck's law for frequency
+    This function calculates the spectral radiance of a black body at a given temperature T for a specific frequency filter. All parameters should be in CGS units
+    
+    
+    Parameters
+    ----------
+    filter : array-like 
+        The frequency values for which to calculate the spectral radiance.
+    t : float
+        The temperature of the black body in Kelvin. Taken from the if functions depending on velocity of forward shock.
+    Returns
+    -------
+    B : array-like
+        Spectral radiance of the black body at the given frequency and temperature."""
+    B = (((2*h*(filter**3))/(c**2)))*((1/(np.exp((h*filter)/(k*t))-1)))
+    return B
+
+#for when b is greater than 0.03 but less than 1 use wien in graph
+def wien_fxn(filter, t):
+
+    """Wien's law for frequency
+    This function calculates the spectral radiance of a black body at a given temperature T for a specific frequency filter. All parameters should be in CGS units
+    
+    
+    Parameters
+    ----------
+    filter : array-like
+        The frequency values for which to calculate the spectral radiance.
+    t : float
+        The temperature of the black body in Kelvin. Taken from the if functions depending on velocity of forward shock.
+    Returns
+    -------
+    B : array-like
+        Spectral radiance of the black body at the given frequency and temperature."""
+    B = ((2 * h * (filter**3)) / (c**2)) * np.exp(-((h * filter)/(k * t)))
+    return B
+
 
 # Reading and convering the LSST filter data
 
@@ -66,69 +154,9 @@ z_freq = c/z_wave
 b_fs = v_fs / c
 gamma_fs = 1 / np.sqrt(1 - b_fs**2)
 
-def temp_fxn(v_fs, dens, m_p, c, k):
-    """
-    Calculate the temperature t based on velocity of forward shock and other physical parameters.
-
-    Parameters
-    ----------
-    v_fs : float
-        Velocity of the forward shock in m/s.
-    dens : float
-        Density of AGN disk.
-    m_p : float
-        Proton mass.
-    c : float
-        Speed of light.
-    k : float
-        Boltzmann constant.
-
-    Returns
-    -------
-    t : float
-        Calculated temperature.
-    """
-    b_fs = v_fs / c
-    gamma_fs = 1 / np.sqrt(1 - b_fs**2)
-    gamma_fs_f = gamma_fs**(1 + np.sqrt(3))
-    gamma_sf = gamma_fs / np.sqrt(2)
-    gamma_sf_f = gamma_sf**(1 + np.sqrt(3))
-    n = dens / m_p
-
-    if b_fs * gamma_fs <= 0.03:
-        t = 1e4 * ((b_fs / 0.02)**0.5) * ((dens / 1e-16)**0.25)
-    elif b_fs * gamma_fs > 0.03 and b_fs * gamma_fs <= 1:
-        t = (10**(0.975 + (1.735 * ((b_fs / 0.1)**0.5)) + ((0.26 - 0.08 * ((b_fs / 0.1)**0.5)) * np.log10(n / 1e15)))) * 1e4
-    elif b_fs * gamma_fs > 1:
-        t = (gamma_sf_f * 5e4) / k
-    if b_fs >= 1:
-        print("Velocity is greater than speed of light, using beta = 0.98 instead")
-        b_fs = 0.98
-        gamma_fs = 1 / np.sqrt(1 - b_fs**2)
-        gamma_sf = gamma_fs / np.sqrt(2)
-        gamma_sf_f = gamma_sf**(1 + np.sqrt(3))
-        t = (gamma_sf_f * 5e4) / k
-    return t
-
-# Plancks law for frequency
 t = temp_fxn(v_fs, dens, m_p, c, k)  # Calculate temperature based on forward shock velocity and density
 
-def planck_fxn(filter, t):
-    """Planck's law for frequency
-    This function calculates the spectral radiance of a black body at a given temperature T for a specific frequency filter.
-    
-    
-    Parameters
-    ----------
-    filter (array-like): The frequency values for which to calculate the spectral radiance.
-    T (float): The temperature of the black body in Kelvin. Taken from the if functions depending on velocity of forward shock.
-    Returns
-    -------
-    B : array-like
-        Spectral radiance of the black body at the given frequency and temperature."""
-    B = (((2*h*(filter**3))/(c**2)))*((1/(np.exp((h*filter)/(k*t))-1)))
-    return B
-
+# Planck's law for frequency (for when b_fs * gamma_fs <= 0.03)
 planck_r = planck_fxn(r_freq,t)
 planck_g = planck_fxn(g_freq,t)
 planck_i = planck_fxn(i_freq,t) 
@@ -173,42 +201,26 @@ total_wave = total_wave[total_index]
 planck_total = (planck_total[total_index])  
 
 # planck law based on area
-planck_area = (planck_total * pi * (rad*2))
+planck_area = (planck_total * pi * (rad**2))
 
 # planck law based on distance 
 planck_dist = (planck_area/(4 * pi * (d**2)))
 
 #t_mid = 1e4
-#for when b is greater than 0.03 but less than 1 use wien
-def wien_fxn(filter, T = t):
 
-    """Wien's law for frequency
-    This function calculates the spectral radiance of a black body at a given temperature T for a specific frequency filter.
-    
-    
-    Parameters
-    ----------
-    filter (array-like): The frequency values for which to calculate the spectral radiance.
-    T (float): The temperature of the black body in Kelvin. Taken from the if functions depending on velocity of forward shock.
-    Returns
-    -------
-    B : array-like
-        Spectral radiance of the black body at the given frequency and temperature."""
-    B = ((2 * h * (filter**3)) / (c**2)) * np.exp(-((h * filter)/(k * T)))
-    return B
-
-wien_r = wien_fxn(r_freq)
-wien_g = wien_fxn(g_freq)
-wien_i = wien_fxn(i_freq) 
-wien_u = wien_fxn(u_freq)
-wien_y = wien_fxn(y_freq)
-wien_z = wien_fxn(z_freq)
+# Wien's law for frequency (used when b_fs * gamma_fs > 0.03)
+wien_r = wien_fxn(r_freq, t)
+wien_g = wien_fxn(g_freq, t)
+wien_i = wien_fxn(i_freq, t) 
+wien_u = wien_fxn(u_freq, t)
+wien_y = wien_fxn(y_freq, t)
+wien_z = wien_fxn(z_freq, t)
 
 wien_freq = np.concatenate((wien_r, wien_g, wien_i, wien_u, wien_y, wien_z)) #combining all the wien values
 wien_total = (wien_freq[total_index])  # sorting the wien values 
 
 # wien law based on area
-wien_area = (wien_total * pi * (rad*2))
+wien_area = (wien_total * pi * (rad**2))
 
 # wien law based on distance 
 wien_dist = (wien_area/(4 * pi * (d**2)))
