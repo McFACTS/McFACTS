fe362da5cc74f9ec9f8377e4e15fa3a3aba024d5
nrposner <nicolasposner@gmail.com>
2026-01-17 12:07:05 -0500

    adding tau_inc_dyn_helper

--- src/mcfacts/physics/disk_capture.py
+++ src/mcfacts/physics/disk_capture.py
@@ -6,6 +6,7 @@ import astropy.constants as const
 import astropy.units as u
 from mcfacts.mcfacts_random_state import rng
 from mcfacts.physics.point_masses import si_from_r_g
+from mcfast import tau_inc_dyn_helper
 
 
 def orb_inc_damping(smbh_mass, disk_bh_retro_orbs_a, disk_bh_retro_masses, disk_bh_retro_orbs_ecc,
@@ -391,6 +392,49 @@ def retro_bh_orb_disk_evolve(smbh_mass, disk_bh_retro_masses, disk_bh_retro_orbs
 
     return disk_bh_retro_orbs_ecc_new, disk_bh_retro_orbs_a_new, disk_bh_retro_orbs_inc_new
 
+def tau_inc_dyn_optimized(smbh_mass, disk_bh_retro_orbs_a, disk_bh_retro_masses, omega, ecc, inc, disk_surf_density_func, r_g_in_meters):
+    """Computes inclination damping timescale from actual variables; used only for scaling.
+    Uses Rust-accelerated helper functions for the calculation: compare to tau_inc_dyn
+
+    Parameters
+    ----------
+    smbh_mass : float
+        Mass [M_sun] of supermassive black hole
+    disk_bh_retro_orbs_a : numpy.ndarray
+        Orbital semi-major axes [r_{g,SMBH}] of retrograde singleton BH at start of a timestep (math:`r_g=GM_{SMBH}/c^2`) with :obj:`float` type
+    disk_bh_retro_masses : numpy.ndarray
+        Mass [M_sun] of retrograde singleton BH at start of timestep_duration_yr with :obj:`float` type
+    disk_bh_retro_arg_periapse : numpy.ndarray
+        Argument of periapse [radian] of retrograde singleton BH at start of a timestep with :obj:`float` type
+    disk_bh_retro_orbs_ecc : numpy.ndarray
+        Orbital eccentricity [unitless] of retrograde singleton BH at start of a timestep with :obj:`float` type
+    disk_bh_retro_orbs_inc : numpy.ndarray
+        Orbital inclination [radian] of retrograde singleton BH at start of a timestep with :obj:`float` type
+    disk_surf_density_func : function
+        Returns AGN gas disk surface density [kg/m^2] given a distance [r_{g,SMBH}] from the SMBH
+    r_g_in_meters: float
+        Gravitational radius of the SMBH in meters
+
+    Returns
+    -------
+    tau_i_dyn : numpy.ndarray
+        Inclination damping timescale [s]
+    """
+    # throw most things into SI units (that's right, ENGINEER UNITS!)
+    #    or more locally convenient variable names
+    SI_smbh_mass = smbh_mass * u.Msun.to("kg")  # kg
+    SI_semi_maj_axis = si_from_r_g(smbh_mass, disk_bh_retro_orbs_a, r_g_defined=r_g_in_meters).to("m").value
+    SI_orbiter_mass = disk_bh_retro_masses * u.Msun.to("kg")  # kg
+    cos_omega = np.cos(omega)
+
+    disk_surf_res = disk_surf_density_func(disk_bh_retro_orbs_a)
+
+    tau_i_dyn = tau_inc_dyn_helper(SI_smbh_mass, SI_orbiter_mass, ecc, inc, cos_omega, disk_surf_res, SI_semi_maj_axis)
+
+    assert np.isfinite(tau_i_dyn).all(), \
+        "Finite check failure: tau_i_dyn"
+
+    return tau_i_dyn
 
 def tau_inc_dyn(smbh_mass, disk_bh_retro_orbs_a, disk_bh_retro_masses, disk_bh_retro_arg_periapse,
                 disk_bh_retro_orbs_ecc, disk_bh_retro_orbs_inc, disk_surf_density_func, r_g_in_meters):
