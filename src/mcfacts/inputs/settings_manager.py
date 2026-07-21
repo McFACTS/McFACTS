a8a4dbd9ba7df8b5b6f783487f301f08ab97cb18
Vera Delfavero <xevra86@gmail.com>
2026-07-21 13:21:07 -0400

    cleaned up scaling options

--- src/mcfacts/inputs/settings_manager.py
+++ src/mcfacts/inputs/settings_manager.py
@@ -195,10 +195,8 @@ DEFAULT_SETTINGS: list[SettingsProperty | StaticSettingsProperty] = [
         SettingsProperty("nsc_imf_bh_method", "nsc", "default", str),
 
         # Scaling settings
-        #SettingsProperty("disk_truncation", "scale", "none", str),
-        SettingsProperty("disk_truncation", "scale", "opacity-equals-inner-disk", str),
-        #SettingsProperty("flag_use_scaling", "scale", False, bool),
-        SettingsProperty("flag_use_scaling", "scale", True, bool),
+        SettingsProperty("disk_truncation", "scale", "none", str),
+        SettingsProperty("flag_use_scaling", "scale", False, bool),
         SettingsProperty("stellar_mass", "scale", 1e10, float),
         SettingsProperty("scale_smbh_mass", "scale", "schramm-silverman", str),
         SettingsProperty("scale_nsc_mass", "scale", "neumayer-early", str),
