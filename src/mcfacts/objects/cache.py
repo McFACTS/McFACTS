076b8f48b6233e03b6a8337cb7d0c512605a3bc9
Vera Delfavero <xevra86@gmail.com>
2026-09-03 09:33:25 -0400

    feat: simplified cache

--- src/mcfacts/objects/cache.py
+++ src/mcfacts/objects/cache.py
@@ -6,10 +6,6 @@ from functools import cached_property as cached_property
 ######## cached_property ########
 class readonly_cached_property(cached_property):
     """Custom readonly cached property"""
-    def __init__(self, func):
-        super().__init__(func)
-        self.is_set = True
-
     def __set__(self, instance, value):
-        raise AttributeError("Property is readonly")
+        raise AttributeError("Property is readonly!")
 
