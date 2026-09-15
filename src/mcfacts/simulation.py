59f8d1914b60391c338912d47fcb13b31de9c3aa
Vera Delfavero <xevra86@gmail.com>
2026-09-15 14:46:40 -0400

    wip: Pulled simulation.main apart and checked that outputs are identical

--- src/mcfacts/simulation.py
+++ src/mcfacts/simulation.py
@@ -32,10 +32,119 @@ from mcfacts.objects.timeline import SimulationTimeline
 
 #### Methods
 def run_galaxy(
-        settings : SettingsManager,
+        settings    : SettingsManager,
+        galaxy      : Galaxy,
+        agn_disk    : AGNDisk = None,
     ):
     """Run an individual galaxy which has already been initialized"""
-    galaxy_seed = settings.seed - galaxy_id
+
+    # Hard checks on types
+    if not isinstance(settings, SettingsManager):
+        raise TypeError(
+            f"settings is type {type(settings)}; "
+            "should be SettingsManager."
+        )
+    if not isinstance(galaxy, Galaxy):
+        raise TypeError(
+            f"galaxy is type {type(galaxy)}; "
+            "should be Galaxy."
+        )
+
+    # Check disk; VD: this could be useful for stuff I have planned
+    if agn_disk is None:
+        agn_disk = AGNDisk(settings)
+
+    # Create timeline to classify objects created during population
+    pre_timeline = SimulationTimeline("Reclassification", timesteps=1, timestep_length=0)
+
+    # Run stars reclassification first, since it can convert stars to bh under certain conditions
+    pre_timeline.add_timeline_actor(InitialStarReclassification())
+    pre_timeline.add_timeline_actor(InitialBlackHoleReclassification())
+    pre_timeline.add_timeline_actor(SingleBlackHoleRealityCheck())
+
+    galaxy.run(pre_timeline, agn_disk)
+
+    # Create timeline to run main simulation
+    active_phase_timeline = SimulationTimeline(
+        "Active Timeline",
+        timesteps=settings.active_timestep_num,
+        timestep_length=galaxy.settings.active_timestep_duration_yr,
+    )
+
+    # Initial check to make sure our single black holes are real
+    active_phase_timeline.add_timeline_actor(SingleBlackHoleRealityCheck())
+
+    # Get names of different singleton arrays we run through the same module
+    # Single retrograde evolution is currently handled separately and not passed as a target array.git
+    prograde_array = galaxy.settings.bh_prograde_array_name
+    innerdisk_array = galaxy.settings.bh_inner_disk_array_name
+    inner_gw_only_array = galaxy.settings.bh_inner_gw_array_name
+
+    # Single Object Physics
+    active_phase_timeline.add_timeline_actors([
+        ProgradeBlackHoleMigration(target_array=innerdisk_array),
+        ProgradeBlackHoleMigration(target_array=prograde_array),
+        SingleBlackHoleRealityCheck(),
+
+        ProgradeBlackHoleAccretion(target_array=innerdisk_array),
+        ProgradeBlackHoleAccretion(target_array=prograde_array),
+        ProgradeBlackHoleDamping(target_array=innerdisk_array),
+        ProgradeBlackHoleDamping(target_array=prograde_array),
+
+        EvolveRetrogradeBlackHoles(),
+        SingleBlackHoleRealityCheck(),
+
+        InnerBlackHoleDynamics(target_array=innerdisk_array),
+        InnerBlackHoleDynamics(target_array=inner_gw_only_array),
+        SingleBlackHoleDynamics(target_array=innerdisk_array),
+        SingleBlackHoleDynamics(target_array=prograde_array),
+    ])
+
+    # Binary Object Physics
+    active_phase_timeline.add_timeline_actors([
+        BinaryBlackHoleDamping(),
+
+        BinaryBlackHoleDynamics(reality_merge_checks=False),
+        ProcessBinaryBlackHoleMergers(),
+
+        BinaryBlackHoleEccDynamics(reality_merge_checks=False),
+        ProcessBinaryBlackHoleMergers(),
+
+        BinaryBlackHoleGasHardening(reality_merge_checks=False),
+        ProcessBinaryBlackHoleMergers(),
+
+        BinaryBlackHoleAccretion(reality_merge_checks=False),
+        ProcessBinaryBlackHoleMergers(),
+
+        BinaryBlackHoleSpheroidDynamics(reality_merge_checks=False),
+        ProcessBinaryBlackHoleMergers(),
+
+        RecaptureBinaryBlackHoles(),
+        BinaryBlackHoleMigration(),
+        BinaryBlackHoleRealityCheck(),
+
+        BinaryBlackHoleEvolveGW(),
+
+        BinaryBlackHoleIonization(),
+        ProcessBinaryBlackHoleMergers(),
+
+        BinaryBlackHoleFormation()
+    ])
+
+    # Create new prograde black holes
+    active_phase_timeline.add_timeline_actor(CaptureNSCProgradeBlackHoles())
+
+    # Population Filters
+    active_phase_timeline.add_timeline_actor(InnerDiskFilter())
+    active_phase_timeline.add_timeline_actor(FlipRetroProFilter())
+
+    # Handle EMRI Dynamics
+    active_phase_timeline.add_timeline_actor(ProcessEMRIMergers())
+
+    # Rub the active timeline
+    galaxy.run(active_phase_timeline, agn_disk)
+
+    #### NOTE STOP ####
 
 def main(settings: SettingsManager):
     # Check for existing output files and overwrite flags
@@ -74,100 +183,17 @@ def main(settings: SettingsManager):
         single_star_populator = SingleStarPopulator()
         galaxy.populate([single_bh_populator, single_star_populator], agn_disk)
 
-        #### NOTE START ####
-        # Create timeline to classify objects created during population
-        pre_timeline = SimulationTimeline("Reclassification", timesteps=1, timestep_length=0)
-
-        # Run stars reclassification first, since it can convert stars to bh under certain conditions
-        pre_timeline.add_timeline_actor(InitialStarReclassification())
-        pre_timeline.add_timeline_actor(InitialBlackHoleReclassification())
-        pre_timeline.add_timeline_actor(SingleBlackHoleRealityCheck())
-
-        galaxy.run(pre_timeline, agn_disk)
-
-        # Create timeline to run main simulation
-        active_phase_timeline = SimulationTimeline("Active Timeline",
-                                               timesteps=settings.active_timestep_num,
-                                               timestep_length=galaxy.settings.active_timestep_duration_yr)
-
-        # Initial check to make sure our single black holes are real
-        active_phase_timeline.add_timeline_actor(SingleBlackHoleRealityCheck())
-
-        # Get names of different singleton arrays we run through the same module
-        # Single retrograde evolution is currently handled separately and not passed as a target array.git
-        prograde_array = galaxy.settings.bh_prograde_array_name
-        innerdisk_array = galaxy.settings.bh_inner_disk_array_name
-        inner_gw_only_array = galaxy.settings.bh_inner_gw_array_name
-
-        # Single Object Physics
-        active_phase_timeline.add_timeline_actors([
-            ProgradeBlackHoleMigration(target_array=innerdisk_array),
-            ProgradeBlackHoleMigration(target_array=prograde_array),
-            SingleBlackHoleRealityCheck(),
-
-            ProgradeBlackHoleAccretion(target_array=innerdisk_array),
-            ProgradeBlackHoleAccretion(target_array=prograde_array),
-            ProgradeBlackHoleDamping(target_array=innerdisk_array),
-            ProgradeBlackHoleDamping(target_array=prograde_array),
-
-            EvolveRetrogradeBlackHoles(),
-            SingleBlackHoleRealityCheck(),
-
-            InnerBlackHoleDynamics(target_array=innerdisk_array),
-            InnerBlackHoleDynamics(target_array=inner_gw_only_array),
-            SingleBlackHoleDynamics(target_array=innerdisk_array),
-            SingleBlackHoleDynamics(target_array=prograde_array),
-        ])
-
-        # Binary Object Physics
-        active_phase_timeline.add_timeline_actors([
-            BinaryBlackHoleDamping(),
-
-            BinaryBlackHoleDynamics(reality_merge_checks=False),
-            ProcessBinaryBlackHoleMergers(),
-
-            BinaryBlackHoleEccDynamics(reality_merge_checks=False),
-            ProcessBinaryBlackHoleMergers(),
-
-            BinaryBlackHoleGasHardening(reality_merge_checks=False),
-            ProcessBinaryBlackHoleMergers(),
-
-            BinaryBlackHoleAccretion(reality_merge_checks=False),
-            ProcessBinaryBlackHoleMergers(),
-
-            BinaryBlackHoleSpheroidDynamics(reality_merge_checks=False),
-            ProcessBinaryBlackHoleMergers(),
-
-            RecaptureBinaryBlackHoles(),
-            BinaryBlackHoleMigration(),
-            BinaryBlackHoleRealityCheck(),
-
-            BinaryBlackHoleEvolveGW(),
-
-            BinaryBlackHoleIonization(),
-            ProcessBinaryBlackHoleMergers(),
-
-            BinaryBlackHoleFormation()
-        ])
-
-        # Create new prograde black holes
-        active_phase_timeline.add_timeline_actor(CaptureNSCProgradeBlackHoles())
-
-        # Population Filters
-        active_phase_timeline.add_timeline_actor(InnerDiskFilter())
-        active_phase_timeline.add_timeline_actor(FlipRetroProFilter())
-
-        # Handle EMRI Dynamics
-        active_phase_timeline.add_timeline_actor(ProcessEMRIMergers())
-
-        # Rub the active timeline
-        galaxy.run(active_phase_timeline, agn_disk)
+        # Run the galaxy
+        run_galaxy(settings, galaxy, agn_disk=agn_disk)
 
         # Ignore consistency checks on these arrays since they are allowed to have duplicates
         population_cabinet.ignore_consistency_check("blackholes_merged")
         population_cabinet.ignore_consistency_check("blackholes_lvk")
 
         # Grab array names from settings manager
+        prograde_array = galaxy.settings.bh_prograde_array_name
+        innerdisk_array = galaxy.settings.bh_inner_disk_array_name
+        inner_gw_only_array = galaxy.settings.bh_inner_gw_array_name
         bbh_merged_array = galaxy.settings.bbh_merged_array_name
         bbh_lvk_array = galaxy.settings.bbh_gw_array_name
         emri_merged_array = galaxy.settings.emri_array_name
@@ -175,23 +201,40 @@ def main(settings: SettingsManager):
 
         # Sort objects into the final population cabinet containing results from all galaxies
         if bh_ejected_array in galaxy.filing_cabinet:
-            population_cabinet.create_or_append_array("blackholes_ejected", galaxy.filing_cabinet.get_array(bh_ejected_array))
+            population_cabinet.create_or_append_array(
+                "blackholes_ejected",
+                galaxy.filing_cabinet.get_array(bh_ejected_array),
+            )
 
         if bbh_merged_array in galaxy.filing_cabinet:
-            population_cabinet.create_or_append_array("blackholes_merged", galaxy.filing_cabinet.get_array(bbh_merged_array))
+            population_cabinet.create_or_append_array(
+                "blackholes_merged",
+                galaxy.filing_cabinet.get_array(bbh_merged_array),
+            )
 
         if bbh_lvk_array in galaxy.filing_cabinet:
-            population_cabinet.create_or_append_array("blackholes_lvk", galaxy.filing_cabinet.get_array(bbh_lvk_array))
+            population_cabinet.create_or_append_array(
+                "blackholes_lvk", 
+                galaxy.filing_cabinet.get_array(bbh_lvk_array),
+            )
 
         if innerdisk_array in galaxy.filing_cabinet:
-            population_cabinet.create_or_append_array("blackholes_emri", galaxy.filing_cabinet.get_array(innerdisk_array))
+            population_cabinet.create_or_append_array(
+                "blackholes_emri", 
+                galaxy.filing_cabinet.get_array(innerdisk_array),
+            )
 
         if inner_gw_only_array in galaxy.filing_cabinet:
-            population_cabinet.create_or_append_array("blackholes_emri", galaxy.filing_cabinet.get_array(inner_gw_only_array))
+            population_cabinet.create_or_append_array(
+                "blackholes_emri",
+                galaxy.filing_cabinet.get_array(inner_gw_only_array),
+            )
 
         if emri_merged_array in galaxy.filing_cabinet:
-            population_cabinet.create_or_append_array("blackholes_emri", galaxy.filing_cabinet.get_array(emri_merged_array))
-        #### NOTE STOP ####
+            population_cabinet.create_or_append_array(
+                "blackholes_emri", 
+                galaxy.filing_cabinet.get_array(emri_merged_array),
+            )
 
     pbar.close()
 
