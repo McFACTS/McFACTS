c5fb4dba96fe0d3e0a3ee175d3fb902aac5a78eb
Vera <vdfavero@cita.utoronto.ca>
2026-09-20 15:16:16 -0400

    Made default hdf5 snapshots more compressed

--- src/mcfacts/inputs/settings_manager.py
+++ src/mcfacts/inputs/settings_manager.py
@@ -118,8 +118,8 @@ DEFAULT_SETTINGS: list[SettingsProperty | StaticSettingsProperty] = [
         SettingsProperty("output_dir", "io", "./runs", str),
         SettingsProperty("settings_snapshot", "io", "ini", str),
         SettingsProperty("cabinet_snapshot", "io", "txt", str),
-        SettingsProperty("hdf5_snapshot_mode", "io", "column", str),
-        SettingsProperty("hdf5_snapshot_compression", "io", "none", str),
+        SettingsProperty("hdf5_snapshot_mode", "io", "compound", str),
+        SettingsProperty("hdf5_snapshot_compression", "io", "gzip", str),
         OptionalSettingsProperty("settings_file", "io", "", str),
 
         # Simulation Parameters
